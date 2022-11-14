1c1900c2702905894f082fed19dc541a5a3a98be
EduardoAzSouza <EduardoAzSouza@users.noreply.github.com>
2022-11-14 12:24:31 -0300

    new file:   admLogin.html 	new file:   admin/admLogin.php 	new file:   autenticacao.php 	new file:   conexao.php 	new file:   estilos/media-query.css 	new file:   estilos/style.css 	new file:   imagens/gestao.jpg 	new file:   imagens/pexels-phearak.jpg 	new file:   index.html 	new file:   login.php 	new file:   main.js 	new file:   painel.php 	new file:   sair.php

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include('conexao.php'); //inclui o PHP com os dasdos da conexão
+
+//Verifica se os campos estão vazio
+if(empty($_POST['login']) || empty($_POST['senha'])) { 
+	header('Location: index.html');
+	exit();
+}
+
+//previne ataque slinjection
+$usuario = mysqli_real_escape_string($conexao, $_POST['login']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+//busca no banco de dados se confere com os dados do formulario, no caso CPF da table com $usuario
+//inserido e senha da table é $senha inserida
+$query = "select id_user, nome, cpf from usuario where cpf = '{$usuario}' and senha ='{$senha}'";
+
+//executa a query
+$result = mysqli_query($conexao, $query);
+
+// resultado se a query é verdadeira ou não, conta o numero de linha encontrada, onde so será 0 ou 1
+$row = mysqli_num_rows($result);
+
+// se for 1 será direcionada para proxima pagina senão será direcionada para index novamente
+if($row == 1) {
+	$_SESSION['usuario'] = $usuario;
+	header('Location: painel.php');
+	exit();
+} else {
+	$_SESSION['nao_autenticado'] = true;
+	header('Location: index.html');
+	exit();
+}
---
+++ painel.php
@@ -0,0 +1,6 @@
+<?php
+include('autenticacao.php');
+?>
+<h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
+<h2><a href="sair.php">Sair</a></h2>
+
