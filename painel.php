73585e2a23ec681c956728eac8372a0f847cab88
EduardoAzSouza <EduardoAzSouza@users.noreply.github.com>
2022-11-16 16:27:34 -0300

    new file:   estilos/main.css 	modified:   painel.php

--- painel.php
+++ painel.php
@@ -1,56 +1,68 @@
 <?php
 include('autenticacao.php');
 include('conexao.php');
-$resultado = mysqli_query($conexao, "select usuario.*, empresa.nome as empresa from usuario inner join empresa on usuario.empresa_id = empresa.id_emp order by id_user");
+$resultado = mysqli_query($conexao, "select usuario.*, empresa.nome as empresa from usuario inner join empresa on usuario.empresa_id = empresa.id_emp order by nome");
 ?>
-<h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
-<h2><a href="sair.php">Sair</a></h2>
 
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8"/>
-	<title>Listagem de usuário</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Painel do Usuario</title>
+	<link rel="stylesheet" href="estilos/main.css">
+	
 </head>
 <body>
-	<?php if(mysqli_num_rows($resultado) < 1){ exit; } ?>
-	<table width="500" border="1" align="center">
-	<tr>
-		<th>ID</th>
-		<th>Nome</th>
-        <th>CPF</th>
-		<th>CNH</th>
-		<th>Endereço</th>
-		<th>Telefone</th>
-        <th>Carro</th>
-        <th>Empresa</th>
-		<th>Editar/Excluir</th>
-	</tr>
-	<?php while($linha = mysqli_fetch_array($resultado)){
-		$id = $linha["id_user"];
-		$nome = $linha["nome"];
-		$CPF = $linha["cpf"];
-		$CNH = $linha["cnh"];
-		$endereco = $linha["endereco"];
-        $telefone = $linha["telefone"];
-        $carro = $linha["carro"];
-        $empresa = $linha["empresa"];
-		
-		echo"
-		<tr>
-			<td>$id</td>
-			<td>$nome</td>
-			<td>$CPF</td>
-			<td>$CNH</td>
-			<td>$endereco</td>
-            <td>$telefone</td>
-            <td>$carro</td>
-            <td>$empresa</td>
-            
-			<td><a href=\"editar.php?id=$id\">[Editar]</a>
-			| <a href=\"excluir.php?id=$id\">[Excluir]</a></td>
-		<tr>\n";
-	}
-	?>
-	</table>
+	<header>
+		<h2 class="titulo">Olá, <?php echo $_SESSION['usuario'];?></h2>
+		<h2><a href="sair.php">Sair</a></h2>
+	</header>
+	<div id="Busca">
+  		<input type="text" id="cxBusca" placeholder="Buscar..."/>
+  		<img src="search3.png" id="btnBusca" alt="Buscar"/>
+	</div>
+	<main>
+		<?php if(mysqli_num_rows($resultado) < 1){ exit; } ?>
+		<table class="tabela">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>CPF</th>
+					<th>CNH</th>
+					<th>Endereço</th>
+					<th>Telefone</th>
+					<th>Carro</th>
+					<th>Empresa</th>
+				</tr>
+			</thead>
+		<?php while($linha = mysqli_fetch_array($resultado)){
+			$nome = $linha["nome"];
+			$CPF = $linha["cpf"];
+			$CNH = $linha["cnh"];
+			$endereco = $linha["endereco"];
+			$telefone = $linha["telefone"];
+			$carro = $linha["carro"];
+			$empresa = $linha["empresa"];
+			
+			echo"
+			<tr>
+				<td>$nome</td>
+				<td>$CPF</td>
+				<td>$CNH</td>
+				<td>$endereco</td>
+				<td>$telefone</td>
+				<td>$carro</td>
+				<td>$empresa</td>
+			<tr>\n";
+		}
+		?>
+		</table>
+
+	</main>
+	<footer>
+		©CRUD by Eduardo Souza.
+	</footer>
+
 </body>
 </html>
